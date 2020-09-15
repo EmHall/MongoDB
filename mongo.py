ea926131f3cfb9ac3a95474533437211687bb4e6
EmHall <em.cm.hall@gmail.com>
2020-09-15 10:59:58 +0000

    make loops

--- mongo.py
+++ mongo.py
@@ -12,21 +12,35 @@ COLLECTION = "Celebrities"
 def mongo_connect(url):
     try:
         conn = pymongo.MongoClient(url)
-        print("Mongo is connected")
         return conn
     except pymongo.errors.ConnectionFailure as e:
         print("Could not connect to MongoDB: %s") % e
 
+def show_menu():
+print("")
+print("1. Add a record")
+print("2. Find a record by name")
+print("3. Edit a record")
+print("4. Delete a record")
+print("5. Exit")
 
-conn = mongo_connect(MONGO_URI)
+option = input("Enter option: ")
+return option
 
-coll = conn[DATABASE][COLLECTION]
 
-new_doc = {"first": "douglas", "last": "adams", "dob": "11/03.1952", "occupation": "writer". "nationality": "british"}
-
-coll.insert(new_doc)
-
-documents = coll.find()
-
-for doc in documents:
-    print(doc)
+def main_loop():
+    while True:
+        option = show menu()
+        if option == "1":
+            print("You have selected option 1")
+        elif option == "2":
+            print("You have selected option 2")
+        elif option == "3":
+            print("You have selected option 3")
+        elif option == "4":
+            print("You have selected option 4")
+        elif option == "5":
+            print("You have selected option 5")
+        else:
+            print("Invalid option")
+        print("")
