d1f8e69f5cda1c5efa7ff53cd5c96a16dbfad88f
EmHall <em.cm.hall@gmail.com>
2020-09-15 13:45:36 +0000

    celebrities database complete

--- mongo.py
+++ mongo.py
@@ -6,41 +6,23 @@ if os.path.exists("env.py"):
 
 MONGO_URI = os.environ.get("MONGO_URI")
 DATABASE = "myFirstDB"
-COLLECTION = "Celebrities"
+COLLECTION = "celebrities"
 
 
 def mongo_connect(url):
     try:
         conn = pymongo.MongoClient(url)
+        print("Mongo is connected")
         return conn
     except pymongo.errors.ConnectionFailure as e:
         print("Could not connect to MongoDB: %s") % e
 
-def show_menu():
-print("")
-print("1. Add a record")
-print("2. Find a record by name")
-print("3. Edit a record")
-print("4. Delete a record")
-print("5. Exit")
 
-option = input("Enter option: ")
-return option
+conn = mongo_connect(MONGO_URI)
 
+coll = conn[DATABASE][COLLECTION]
 
-def main_loop():
-    while True:
-        option = show menu()
-        if option == "1":
-            print("You have selected option 1")
-        elif option == "2":
-            print("You have selected option 2")
-        elif option == "3":
-            print("You have selected option 3")
-        elif option == "4":
-            print("You have selected option 4")
-        elif option == "5":
-            print("You have selected option 5")
-        else:
-            print("Invalid option")
-        print("")
+documents = coll.find()
+
+for doc in documents:
+    print(doc)
